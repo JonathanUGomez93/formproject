e6e045d9efccde069cd76b01b38a98cfaf736c4f
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-09-09 08:26:50 -0300

    navbar setup

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import '../../styles/navbar.css'
 
 const Navbar = () => {
     return (
-        <ul>
+        <div className="navbar">
             <Link to="/">Home</Link>
-            <Link to="/register">Registrate</Link>
+            <Link to="/contact">Contacto</Link>
             <Link to="/login">Inicia Sesión</Link>
-            <Link to="/us">Nosotros</Link>
-        </ul>
+            <Link to="/register">Registrate</Link>
+        </div>
     )
 }
 

cb7e1dc222d14ab6079b5b04d522698710c9650e
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-09-10 12:39:17 -0300

    fix

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <>
             <h1>Grana Store</h1>
             <div className='productsPreview'>
-                <img src="../images/train5.png" alt="" />
+                <img src="../images/train5.png" alt="camiseta lanus" />
                 <img src="../images/train6.png" alt="" />
                 <img src="../images/train7.png" alt="" />
                 <img src="../images/train4.png" alt="" />

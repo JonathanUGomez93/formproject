f1499823d8ebf133d1d85c3976cf37dcd61b35d5
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-09-17 19:08:48 -0300

    few minor changes

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <Link to="/">Home</Link>
-            <Link to="/contact">Contacto</Link>
-            <Link to="/login">Inicia Sesión</Link>
-            <Link to="/register">Registrate</Link>
+            <Link to="/products">Productos</Link>
+            <Link to="/account">Cuenta</Link>
         </div>
     )
+    //dropdown en productos separado por tipo de equipacion y cuenta llega a una pagina compartida de logueo/registro.
 }
 
 export default Navbar

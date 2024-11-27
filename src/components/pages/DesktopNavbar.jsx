0a3e57c089c460ae4f4db3d42d9500b010ba1ef8
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-11-27 18:30:37 -0300

    base logic cart number on navbar being real

--- src/components/pages/DesktopNavbar.jsx
+++ src/components/pages/DesktopNavbar.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import '../../styles/desktopNavbar.css';
+import { context } from '../CustomProvider'
+import { useContext } from 'react'
 
 function DesktopNavbar() {
+  const { loguedUser } = useContext(context);
+  console.log(loguedUser)
+
+  const cartValue = loguedUser.cart.length
+  //ponerle el  useeffect a cartValue para que se redibuje cada vez que loguedUser.cart se modifique
+
   return (
     <div className="desktopNavbar">
       <Link className="navigation" to="/">Home</Link>
@@ -21,7 +29,7 @@ function DesktopNavbar() {
         <div className="dropdown">
           <div className="dropdownOptions">
             <Link to="/account">Mi Cuenta</Link>
-            <Link to="/cart">Carrito(0)</Link>
+            <Link to="/cart">Carrito ({cartValue.lenght === 0 ? cartValue : "0"})</Link>
           </div>
         </div>
       </div>
